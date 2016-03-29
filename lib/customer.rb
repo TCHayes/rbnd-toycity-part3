f72a5d12c6a3adcffafa7cfcba6ed10a78b3d6a8
Terry Hayes <terrychayes@gmail.com>
2016-03-29 18:17:58 -0400

    added Customer class and DuplicateCustomerError.

---
+++ lib/customer.rb
@@ -0,0 +1,29 @@
+class Customer
+	attr_reader :name
+
+	@@customers = []
+
+	def initialize(options = {})
+		@name = options[:name]
+		add_to_customers
+	end
+
+	def self.all
+		@@customers
+	end
+
+    def self.find_by_name(name)
+        @@customers.find {|customer| customer.name == name}
+    end
+
+    private
+
+    def add_to_customers
+        if @@customers.to_s.include? self.name
+            raise DuplicateCustomerError.new, "#{self.name} already exists."
+        else
+            @@customers << self
+        end
+    end
+
+end
--- lib/product.rb
+++ lib/product.rb
@@ -1,16 +1,36 @@
 class Product
 	attr_reader :title
+	attr_reader :price
+	attr_reader :stock
 
  
     @@products = []
 
     def initialize(options={})
         @title = options[:title]
+        @price = options[:price]
+        @stock = options[:stock]
         add_to_products
     end
  
     def self.all
         @@products
+    end
+
+    def self.find_by_title(title)
+        @@products.find {|product| product.title == title}
+    end
+
+    def in_stock?
+        if self.stock > 0
+            true
+        else
+            false
+        end
+    end    
+
+    def self.in_stock
+        @@products.find_all { |product| product.in_stock? == true}
     end
 
     private
